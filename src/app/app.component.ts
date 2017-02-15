36ea28f778c80d498248bc2990b7406cc2c3bf5e
Ranu <O@O.net>
2017-02-15 11:12:18 +0000

    Add Property Binding Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',  // to use me, do this in html <my-app>
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>{{name}}</h1>
+  <fieldset>
+    <img src={{image}}>
+    <img [src]="image">
+  </fieldset>
+  <label [style.color]="colour">Favourite Colour</label>
+  `,
 })
 export class AppComponent {
-  name = 'Angular X';
+  name = 'Alex Smith';
+  image = 'favicon.ico';
+  colour = 'red';
 }
