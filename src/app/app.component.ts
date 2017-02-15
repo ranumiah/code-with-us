4ba2c58774c8f7e9bb49e0c22f7c6675e9209f3c
Ranu <O@O.net>
2017-02-15 12:37:16 +0000

    Add Event Binding Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,41 @@ import { Component } from '@angular/core';
   selector: 'my-app',  // to use me, do this in html <my-app>
   template: `
   <h1>{{name}}</h1>
-  <fieldset>
-    <img src={{image}}>
-    <img [src]="image">
-  </fieldset>
-  <label [style.color]="colour">Favourite Colour</label>
+
+  <div>
+    <fieldset>
+      <img src={{image}}>
+      <!-- img is an Element of HTML and src is the source of the element -->
+      <img [src]="image">
+    </fieldset>
+  </div>
+
+  <div>
+    <label [style.color]="colour">Favourite Colour</label>
+    <button (click)="clicked()">Toggle Colour</button>
+<!--
+    <select #selector (change)="colourChange(selector.value)"> ==> This is a Template Variable that refers to itself
+    <select (change)="colourChange($event.target.value)"> ==> $event is a DOM event and this does the same as above
+-->
+    <select (change)="colourChange($event.target.value)">
+      <option>red</option>
+      <option>blue</option>
+      <option>yellow</option>
+    </select>
+  </div>
   `,
 })
+
 export class AppComponent {
-  name = 'Alex Smith';
+  name = 'Wu Joe';
   image = 'favicon.ico';
   colour = 'red';
+
+  clicked() {
+    this.colour = this.colour === 'red' ? 'blue' : 'red';
+  }
+
+  colourChange(colour: string) {
+    this.colour = colour;
+  }
 }
