13555d88171829cf59b513fc888314c1e8765383
Ranu <O@O.net>
2017-02-15 17:10:58 +0000

    Add *ngFor - List Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,59 @@ import { Customer } from './model';
 })
 
 export class AppComponent {
-  customer: Customer = {
-    id: 1,
-    name: 'John Smith',
-    address: {
-      street: '221A Baker Street',
-      city: 'City of London',
-      county: 'Greater London',
-      region: 'Capital'
-    }
-  };
+  customers: Customer[] = [
+    {
+      id: 1,
+      name: 'John Smith',
+      address: {
+        street: '221A Baker Street',
+        city: 'City of London',
+        county: 'Greater London',
+        region: 'Capital'
+      }
+    },
+    {
+      id: 2,
+      name: 'Alex Smith',
+      address: {
+        street: '123 Main Street',
+        city: 'Shrewsbury',
+        county: 'Shropshire',
+        region: 'West'
+      }
+    },
+    {
+      id: 3,
+      name: 'Pierre Pasmal',
+      address: {
+        street: '456 Rue de Main',
+        city: 'Boston',
+        county: 'Lincolnshire',
+        region: 'East'
+      }
+    },
+    {
+      id: 4,
+      name: 'Margarita Nadie',
+      address: {
+        street: '789 Calle Principal',
+        city: 'Basingstoke ',
+        county: 'Hampshire',
+        region: 'South'
+      }
+    },
+    {
+      id: 5,
+      name: 'Katie O\'Leary',
+      address: {
+        street: '137 DeKoven Street',
+        city: 'Morpeth',
+        county: 'Northumberland',
+        region: 'North'
+      }
+    },
+  ];
+  regions = ['North', 'East', 'South', 'West', 'Capital'];
+  customer: Customer = this.customers[0];
   hideAdress = false;
 }
