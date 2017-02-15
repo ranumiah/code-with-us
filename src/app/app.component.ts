94fd099561b760624c1aa22696ba514c96a875b1
Ranu <O@O.net>
2017-02-15 14:56:50 +0000

    Add ngModel to do Two Way Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,34 @@ import { Component } from '@angular/core';
   <h1>{{name}}</h1>
   <p><i>{{name}} is in the {{region}} region</i></p>
   <br />
+
+  <fieldset>
+    <label>Name: </label><br/>
+    <input [value]="name"><br/> <!-- One Way Binding -->
+
+    <!-- Two Different Approach Doing Two Way Binding -->
+    <input #namebox [value]="name" (input)="name=namebox.value"><br/>
+    <input [value]="name" (input)="name=$event.target.value"><br/>
+    
+    <input [value]="name" (keyup)="name=$event.target.value"><br/> <!-- Two Way Binding -->
+
+    <!-- Two Way Binding after pressing the ENTER ONLY-->
+    <input [value]="name" (keyup.enter)="name=$event.target.value"><br/>
+
+    <!-- Two Way Binding after pressing the ENTER OR LOST FOCUS-->
+    <input [value]="name"
+          (keyup.enter)="name=$event.target.value"
+          (blur)="name=$event.target.value"
+    ><br/>
+
+    <input [(ngModel)]="name"><br/> <!-- Same As Line 13 & 14 -->
+    <input [ngModel]="name" (ngModelChange)="name=$event"><br/> <!-- Two Way Binding -->
+  </fieldset>
+
+  <!-- Two Different Approach Doing The Same -->
   <button (click)="addressClick()">Show/Hide Address</button>
+  <label><input type="checkbox" [(ngModel)]="hideAdress"> Hide Address</label>
+
   <div [hidden]="hideAdress" >
     <fieldset>
       <label>Street: </label>{{street}}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 // So this guy describes the class that follows it
 @NgModule({
-  imports: [BrowserModule],  // what stuff do i need?
+  imports: [BrowserModule, FormsModule],  // what stuff do i need?
   declarations: [AppComponent],  // what things are in my app
   bootstrap: [AppComponent]  // where do i start?
 })
